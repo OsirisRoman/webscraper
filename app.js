ff96d9c5b5e93257c41a991c274bb53b17503351
Osiris Román <osirisr1994@gmail.com>
2021-04-23 15:19:34 -0500

    An stable version of the web scrapper
    In this case the problem is solved using headless: false.
    Headless: true retrieves a captcha page that could no be bypassed yet.

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  // const browser = await puppeteer.launch({
-  //   headless: false,
-  //   devtools: true,
-  //   defaultViewport: null,
-  // });
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    slowMo: 10,
+  });
+
   const page = await browser.newPage();
-  //page.on("console", msg => console.log("PAGE LOG:", msg.text()));
+
   await page.goto("http://www.bolsadesantiago.com/detalle_indice/SP%20IPSA", {
     waitUntil: "domcontentloaded",
     timeout: 0,
   });
+  await page.screenshot({ path: "CheckItIsNotCaptcha.png" });
 
   await page.waitForSelector("[ng-if=boxEmpresasIndice] table tbody a");
 
+  //Retrieve the list of companies of our interest
   let companiesList = await page.evaluate(() => {
     const elements = document.querySelectorAll(
       "[ng-if=boxEmpresasIndice] td a"
@@ -23,18 +25,20 @@ const puppeteer = require("puppeteer");
 
     let listResult = [];
     for (let element of elements) {
-      listResult.push({ name: element.innerText, linkRef: element.href });
+      listResult.push({
+        name: element.innerText,
+        linkRef: element.href.replace("https", "http"),
+      });
     }
     return listResult;
   });
 
+  //Retrieve the stock price of each company in the list
   for (let company of companiesList) {
     await page.goto(company.linkRef, {
-      waitUntil: "domcontentloaded",
-      timeout: 10000,
+      waitUntil: "networkidle0",
     });
 
-    await page.waitForTimeout("5000");
     await page.waitForSelector(".row.mb-3 .col-lg-6.border-right h4");
     const stockValue = await page.evaluate(() => {
       let price = document.querySelectorAll(".row.mb-3 h4")[0].innerText;
@@ -44,15 +48,16 @@ const puppeteer = require("puppeteer");
     company.price = stockValue;
   }
 
+  //Descending order
   companiesList.sort((a, b) =>
-    a.price > b.price ? 1 : b.price > a.price ? -1 : 0
+    a.price > b.price ? -1 : b.price > a.price ? 1 : 0
   );
-
+  //Print just the five ones with highest price.
   for (let i = 0; i < 5; i++) {
     console.log(companiesList[i]);
   }
-  //console.log("--------------------");
-  //console.log(companiesList);
+  // Print The whole list of companies
+  // console.log(companiesList);
 
   await browser.close();
 })();
